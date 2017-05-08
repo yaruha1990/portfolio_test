ba7ddf7f29a5a90c6b6835d9f677df48835cf871
Yaroslav Storozhuk <yaruha@ukr.net>
2017-05-08 15:11:31 +0300

    new pages and functions added

--- contacts.php
+++ contacts.php
@@ -24,9 +24,32 @@ function print_contacts($contacts){
     <?php print_components($components) ?>
 </head>
 <body style="padding: 20px">
-<div class="panel panel-default ">
+<div class="panel panel-default">
     <div class="panel-heading">
-        <span style="padding: 20px">Yaroslav Storozhuk</span>
+        <span style="padding: 20px;font-size: large">Portfolio Yaroslav Storozhuk</span>
+    </div>
+    <div class="panel-heading" align="right">
+        <?php
+        if (!empty($_COOKIE['login'])){
+            echo "<p>You have entered as ".$_COOKIE['login']."</p>"."
+                                <form action=\"exit.php\" method=\"get\">
+                                <input type=\"submit\" value=\"Exit\" class=\"btn btn-success\">
+                                </form>";
+        } else{
+            echo "<form action=\"enter.php\" method=\"request\" role=\"form\" class=\"form-inline\">
+                <div class=\"form-group\">
+                    <label for=\"login\">Login</label>
+                    <input size=\"10\" name=\"login\" type=\"text\" class=\"form-control\" id=\"login\" placeholder=\"Enter your login\">
+                </div>
+                <div class=\"form-group\">
+                    <label for=\"password\">Password</label>
+                    <input size=\"10\" name=\"pass\" type=\"password\" class=\"form-control\" id=\"password\" placeholder=\"Enter your password\">
+                </div>
+                <input type=\"submit\" value=\"Entry\" class=\"btn btn-success\"/> /
+                <a href=\"registration.php\" class=\"btn btn-success\">Registration</a>
+            </form>";
+        }
+        ?>
     </div>
     <div class="panel-body">
         <?php print_menu($menu) ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include 'headandmenu.php'
+include 'headandmenu.php';
 ?>
 
 <!DOCTYPE html>
@@ -14,30 +14,33 @@ include 'headandmenu.php'
             <span style="padding: 20px;font-size: large">Portfolio Yaroslav Storozhuk</span>
         </div>
         <div class="panel-heading" align="right">
-            <form role="form" class="form-inline">
-                <div class="form-group">
-                    <label for="login">Login</label>
-                    <input size="10" type="text" class="form-control" id="login" placeholder="Enter your login">
+            <?php
+                if (!empty($_COOKIE['login'])){
+                    echo "<p>You have entered as ".$_COOKIE['login']."</p>"."
+                                <form action=\"exit.php\" method=\"get\">
+                                <input type=\"submit\" value=\"Exit\" class=\"btn btn-success\">
+                                </form>";
+                } else{
+                    echo "<form action=\"enter.php\" method=\"request\" role=\"form\" class=\"form-inline\">
+                <div class=\"form-group\">
+                    <label for=\"login\">Login</label>
+                    <input size=\"10\" name=\"login\" type=\"text\" class=\"form-control\" id=\"login\" placeholder=\"Enter your login\">
                 </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input size="10" type="password" class="form-control" id="password" placeholder="Enter your password">
+                <div class=\"form-group\">
+                    <label for=\"password\">Password</label>
+                    <input size=\"10\" name=\"pass\" type=\"password\" class=\"form-control\" id=\"password\" placeholder=\"Enter your password\">
                 </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input size="10" type="email" class="form-control" id="email" placeholder="Enter your email">
-                </div>
-                <label for="email">
-                    <input type="checkbox">Remember me
-                </label>
-                <button type="submit" class="btn btn-success">Registration</button>
-            </form>
+                <input type=\"submit\" value=\"Entry\" class=\"btn btn-success\"/> /
+                <a href=\"registration.php\" class=\"btn btn-success\">Registration</a>
+            </form>";
+                }
+            ?>
         </div>
         <div class="panel-body">
             <?php print_menu($menu) ?>
         </div>
     </div>
-</div><br>
+<br>
 </body>
 </html>
 
